262929e279ec9e5650f18ab5e8dee6e5c69ca911
Judah Mendoza <judahramses@gmail.com>
2023-02-11 21:45:02 -0800

    2/11/23 update algo

--- Python/Strings/restoreString.py
+++ Python/Strings/restoreString.py
@@ -1,11 +1,21 @@
 class Solution:
     def restoreString(self, s: str, indices: list[int]) -> str:
-        indices.sort()
         result = ""
-        for i in indices:
-            for j in range(len(s))
-                result += 
+        for i in range(len(s)):
+            result += s[indices.index(i)]
+        return result
+
+    def restoreString2(self, s: str, indices: list[int]) -> str:
+        temp = [''] * len(s)
+        i = 0
+        for j in indices:
+            temp[j] = s[i]
+            i += 1
+        return "".join(temp)
+
 
 
 test1 = Solution()
+test2 = Solution()
 print(test1.restoreString("codeleet", [4,5,6,7,0,2,1,3]))
+print(test2.restoreString2("codeleet", [4,5,6,7,0,2,1,3]))
